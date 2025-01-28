35d2a8774703ee0e63bf655f245a0149fa4dd0ca
Kazehaya-ctrl <mahitomaki96@gmail.com>
2025-01-29 00:38:57 +0530

    upt: testing

--- pushit.cpp
+++ pushit.cpp
@@ -1,5 +1,29 @@
 #include<iostream>
+#include<cstdlib>
+#include<cstring>
+#include<string>
 
-int main() {
-  return 0;
+typedef int i;
+
+i main(i args, char* argv[]) {
+  std::string msgStore = argv[1];
+  std::string command = "git commit -m \"" + msgStore + "\"";
+  const char* commitCommand = command.c_str();
+  i result = system("git add .");
+  if(result  == 0) {
+    i result2 = system(commitCommand);
+    if (result2 == 0) {
+      i result3 = system("git push origin main");
+      if(result3 == 0) {
+        std::cout << "repo Pushed" << std::endl;
+      } else {
+        std::cout << "Error happend in git push" << std::endl;
+      }
+    } else {
+      std::cout << "Error happend in git commit" << std::endl;
+    }
+  } else {
+    std::cout << "Error happend in git add" << std::endl;
+  }
+	return 0;
 }
